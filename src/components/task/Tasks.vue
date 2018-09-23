493ca53570f54b47b4fb77b422496c6698077716
liboskat <rizvanov98@gmail.com>
2018-09-23 14:21:14 +0300

    you sobaki

--- src/components/task/Tasks.vue
+++ src/components/task/Tasks.vue
@@ -1,5 +1,30 @@
 <template>
   <main-layout>
+
+    <v-layout class="mt-4 mx-4">
+
+      <div class="mt-3 ml-3">
+        <v-toolbar-title v-text="title"></v-toolbar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="mr-1 mt-2">
+
+        <v-btn
+          class="ml-4"
+          color="primary"
+          round
+          outline
+          @click="dialog = true"
+        >
+          Добавить задачу
+        </v-btn>
+
+      </div>
+
+    </v-layout>
+
     <!-- table begin -->
     <v-flex xs12 class="px-4">
 
@@ -23,50 +48,149 @@
             {{td.item.attributesCollection.find( elem => elem.name === column) | label}}
           </td>
 
-          <td class="justify-center layout px-0">
-            <v-icon
-              small
-              class="mr-2"
-              @click="runProcess(td.item.objectId)"
-            >
-              play_arrow
-            </v-icon>
-            <v-icon
-              small
-              class="mr-2"
-              @click="showUpdateDialog(td.item)"
-            >
-              edit
-            </v-icon>
-            <v-icon
-              small
-              @click="deleteItem(td.item)"
-            >
-              delete
-            </v-icon>
-          </td>
         </template>
         <!-- end of table items -->
 
       </v-data-table>
 
     </v-flex>
+    <!-- table end -->
+
+    <!-- dialog -->
+    <v-dialog v-model="dialog" width="360">
+      <v-card>
+
+        <v-card-title class="headline" v-text="title">
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <div class="px-3">
+          <v-text-field
+            v-model="newItem.UsrName"
+            label="Название"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="newItem.UsrDescription"
+            label="Описание"
+            required
+          ></v-text-field>
+
+          <v-text-field
+            v-model="newItem.UsrDifficulty"
+            label="Сложность"
+            required
+          ></v-text-field>
+        </div>
+
+
+        <v-card-actions>
+          <v-spacer/>
+          <v-btn
+            flat
+            color="error"
+            @click="resetNewItem()"
+          >
+            Отмена
+          </v-btn>
+
+          <v-btn
+            flat
+            color="primary"
+            @click="saveChanges()"
+          >
+          </v-btn>
+        </v-card-actions>
+
+      </v-card>
+    </v-dialog>
+    <!-- dialog -->
   </main-layout>
 </template>
+
 <script>
   import {mapState} from 'vuex';
+  import Filters from '@/helpers/HelperFilters'
 
   export default {
     name: 'Tasks',
 
+    filters: {
+      split: (income = 0) => {
+        return Filters.splitNumbers(income);
+      },
+      humanDate: (income = 0) => {
+        return Filters.humanDate(income)
+      },
+      label: (item) => {
+        if (item.type && item.type === 'Edm.DateTime') {
+          return Filters.humanDate(item.value)
+        }
+        return item.value
+      }
+    },
+
+    data: () => ({
+      title: 'Список невзятых заданий',
+      dialog: false,
+      newItem: {
+        Id: null,
+        UsrName: null,
+        UsrDescription: null,
+        UsrDifficulty: null
+      },
+      processCode: 'icl1teamTestService',
+      processParams: [
+        {
+          "name": "ProcessSchemaParameter1",
+          "value": "testText",
+          "type": "string"
+        }, {
+          "name": "ProcessSchemaParameter2",
+          "value": "79",
+          "type": "string"
+        }, {
+          "name": "ProcessSchemaParameter3",
+          "value": "79",
+          "type": "integer"
+        }
+      ],
+      editableItem: {},
+      showDatePicker: false
+    }),
+
+    computed: {
+      ...mapState({
+        columns: (state) => state.contacts.columns,
+        items: (state) => state.contacts.contacts
+      }),
+      headers: function () {
+        let result = [];
+        this.columns.forEach((item, index, array) => {
+          result.push({
+            text: item,
+            value: item,
+            sortable: false,
+          })
+        });
+        result.push({
+          text: 'Actions',
+          align: 'left',
+          sortable: false,
+        });
+        return result
+      }
+    },
+
     beforeCreate() {
-      this.$store.dispatch('getTask');
+      this.$store.dispatch('loadContacts');
     },
 
     methods: {
       saveChanges: function () {
-        let action = this.newItem.Id ? 'updateContact' : 'createContact';
-        this.$store.dispatch(action, this.newItem).then(() => {
+        this.$store.dispatch('createTask', this.newItem).then(() => {
           this.resetNewItem();
         });
       },
@@ -74,24 +198,6 @@
         Object.keys(this.newItem).forEach((key) => this.newItem[key] = null);
         this.dialog = false;
       },
-      showUpdateDialog: function (item) {
-        item.attributesCollection.forEach((attr) => {
-          if (this.newItem.hasOwnProperty(attr.name)) {
-            this.newItem[attr.name] = attr.value;
-          }
-        });
-        this.dialog = true;
-      },
-      deleteItem: function (item) {
-        confirm('Вы уверены, что хотите удалить этот элемент?') && this.$store.dispatch('deleteContact', item.objectId);
-      },
-      runProcess: function (objectId = null) {
-        this.$store.dispatch('runProcess', {
-          objectId: objectId,
-          processCode: this.processCode,
-          parameters: this.processParams,
-        })
-      }
     },
   }
 </script>
